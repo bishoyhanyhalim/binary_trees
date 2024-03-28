c797737d29d60431cecce8b32119c94a91ef9b36
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-03-27 18:30:31 -0700

    task 1--

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,12 +12,17 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *create_new_node;
 
-	if (parent == NULL || create_new_node == NULL)
+	if (parent == NULL)
 	{
 		return (NULL);
 	}
 
 	create_new_node = binary_tree_node(parent, value);
+
+	if (create_new_node == NULL)
+	{
+		return (NULL);
+	}
 
 	if (parent->left != NULL)
 	{
