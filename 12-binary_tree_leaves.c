ae629bfea70170a45628c283df181c6431da897a
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-03-27 19:10:06 -0700

    task 12 done

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - this is height fo tree
+ *
+ * @tree: A pointer to pointer for root
+ *
+ * Return: this return the result
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t this_leave = 0;
+
+	if (tree)
+	{
+		this_leave += (!tree->right && !tree->left) ? 1 : 0;
+		this_leave += binary_tree_leaves(tree->right);
+		this_leave += binary_tree_leaves(tree->left);
+	}
+
+	return (this_leave);
+}
