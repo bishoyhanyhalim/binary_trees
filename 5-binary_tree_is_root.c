43c91db6329f61d3551e75835c33bc2009dd109a
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-03-27 18:47:48 -0700

    5

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,19 +1,18 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_root - Checks root node done
+ * binary_tree_is_root - Checks the root
  *
- * @node: A pointer gor
+ * @node: A pointer for binaty
  *
- * Return: for binary root result
+ * Return: for the node result
  */
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node->parent != NULL || node == NULL)
+	if (node == NULL || node->parent != NULL)
 	{
 		return (0);
 	}
-
 	return (1);
 }
