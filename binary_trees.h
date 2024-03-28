55c3536057ff6f95cbc446d9ae8905a67d81eca1
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-03-27 18:18:07 -0700

    test

--- binary_trees.h
+++ binary_trees.h
@@ -24,7 +24,6 @@ typedef struct binary_tree_s
 typedef struct binary_tree_s heap_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
-typedef struct binary_tree_s binary_tree_t;
 
 /* now this is printing binary func */
 
