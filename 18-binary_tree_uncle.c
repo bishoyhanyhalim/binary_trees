d357d5386016e6d5bdaf1e33b60899de5c702431
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-03-27 19:38:32 -0700

    18 alll

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_uncle - this func for binary tree
+ * binary_tree_sibling - this func for binary tree
  *
  * @node: A pointer to tree
  *
@@ -9,7 +9,7 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node->parent == NULL || node == NULL ||
+	if (node == NULL || node->parent == NULL ||
 		node->parent->parent == NULL)
 	{
 		return (NULL);
