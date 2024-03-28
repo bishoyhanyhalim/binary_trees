767482efd1e5cad103756b7da4f916d0666cf662
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-03-27 18:09:39 -0700

    task 1

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,5 @@
-#ifndef BINARY_TREES_H
-#define BINARY_TREES_H
+#ifndef BINARY_TREE
+#define BINARY_TREE
 
 #include <stdlib.h>
 #include <stddef.h>
@@ -26,5 +26,6 @@ typedef struct binary_tree_s
 void binary_tree_print(const binary_tree_t *);
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
 #endif
