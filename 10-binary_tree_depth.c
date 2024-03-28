703d88f66dbe82b899a26c7b0ba25d75befa2f6f
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-03-27 19:06:31 -0700

    task 10 agine

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -3,12 +3,12 @@
 /**
  * binary_tree_depth - this is height fo tree
  *
- * @tree: A pointer for root
+ * @tree: A pointer to pointer for root
  *
  * Return: this return the result
  */
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	return ((tree->parent && tree) ? 1 + binary_tree_depth(tree->parent) : 0);
+	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
 }
